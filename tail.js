a03301cc61460f9efa383f504019929c48041d3d
nicoayamen <nicolas.ayala4@gmail.com>
2024-01-25 21:23:37 -0700

    created tail func that takes the indexes after 0 and does not mutate og array

---
+++ tail.js
@@ -0,0 +1,26 @@
+// the assertEqual func
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// takes the indexes after [0] and does not mutate array
+const tail = function(insert) {
+  return insert.slice(1);
+};
+
+//test case 1
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+//test case 2
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(words); //checking to see original array
+console.log(tail(words)); // checking return in new array
+assertEqual(words.length, 3); // original array should still have 3 elements!
