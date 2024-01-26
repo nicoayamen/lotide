81d1101b8a4567f60b4d7d6a2da6bd814a890248
nicoayamen <nicolas.ayala4@gmail.com>
2024-01-25 19:51:36 -0700

    created assertEquals.js as a verbose way to check truthy or not

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// assert function implementation
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('Assertion Passed:' + actual + ' === ' + expected);
+  } else {
+    console.log('Assertion Failed:' + actual + ' !== ' + expected);
+  }
+
+};
+
+assertEqual("booty", "Booty");
+assertEqual(7, -7);
