04958940e527503285110fcea8d3204cd7d9e9c9
nicoayamen <nicolas.ayala4@gmail.com>
2024-01-27 22:29:29 -0700

    moved length checker outside loop

--- eqArrays.js
+++ eqArrays.js
@@ -9,11 +9,11 @@ const assertEqual = function(actual, expected) {
 
 // func to show if two arrs have equal elements/length
 const eqArrays = function(arr1, arr2) {
+  // checks to see arrs do not have the same length
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; arr1.length > i; i++) {
-    // checks to see arrs do not have the same length
-    if (arr1.length !== arr2.length) {
-      return false;
-    }
     // checks to see if, as the arrs are looping, are not equal
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -25,4 +25,4 @@ const eqArrays = function(arr1, arr2) {
 };
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, "2", 3]), true);
