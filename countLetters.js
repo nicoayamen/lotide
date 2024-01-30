b4e79aa56974cc65957616a06733b6c2908a9377
nicoayamen <nicolas.ayala4@gmail.com>
2024-01-29 20:51:39 -0700

    forgot to change letters to chars wtf

--- countLetters.js
+++ countLetters.js
@@ -13,11 +13,11 @@ const countLetters = function(string) {
     // to not include spaces and only count and add letters. if it includes a space, it will not be added.
     if (chars !== " ") {
       // to check if a letter exists in obj, if it does add 1
-      if (obj[letters]) {
-        obj[letters] += 1;
+      if (obj[chars]) {
+        obj[chars] += 1;
         // if a new letter comes into obj, set to 1
       } else {
-        obj[letters] = 1;
+        obj[chars] = 1;
       }
     }
   }
