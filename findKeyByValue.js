d2526db1456ddf6e38cb49f5f080f97dfc3eaa91
nicoayamen <nicolas.ayala4@gmail.com>
2024-01-30 18:36:13 -0700

    created func to determine the key of value and return key string

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const findKeyByValue = function(genres, show){
+  // since returning a key, which is a string results must be undefined
+  let result = undefined;
+  // use in to loop over obj keys
+  for (const genre in genres) {
+    // if the show we are looking for exists in the genres obj return the key of the valuse
+    if (show === genres[genre]) {
+        result = genre;
+    }
+  }
+  return result;
+};
+
+
+
+
+
+
+// test cases
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
