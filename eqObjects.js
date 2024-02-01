9cf3391a46b87d50ba0f83091bb1b06af97c49bc
nicoayamen <nicolas.ayala4@gmail.com>
2024-01-31 20:24:29 -0700

    added cond to check if objects have same keys and values, just in different places in obj

--- eqObjects.js
+++ eqObjects.js
@@ -35,19 +35,22 @@ const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"
 const eqObjects = function(obj1, obj2) {
   const arr1 = Object.keys(obj1);
   const arr2 = Object.keys(obj2);
-  
-  
-  if (!eqArrays(arr1, arr2)) {
-    return false;
+  //console.log(arr1, arr2);
+
+  // if the keys are equal,
+  if (eqArrays(arr1, arr2)) {
+    if (!eqArrays(arr1, arr2)) {
+      return false;
+    }
   }
-  
-  
+
+
   for (key of arr1) {
     console.log("Hello");
     // cond 1 if the value is an array, and second val is an array, then we want to compare the arrays
     //if theyre not arrays
     if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-        if (!eqArrays(obj1[key], obj2[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) {
         return false;
       }
       continue;
@@ -57,31 +60,20 @@ const eqObjects = function(obj1, obj2) {
     }
   }
   return true;
-
-  /*if (arr1.length !== arr2.length) {
-    return false;
-  } for (loop1 of arr1) {
-    if (Array.isArray(arr1) && Array.isArray(arr2)) {
-      return eqArrays(arr1, arr2) == true;
-    } else if (arr1[loop1] === arr2[loop1]) {
-      return true;
-    } else {
-      return false;
-    }
-  } */
 };
 
 const example = {
+  fizz: 'buzzz',
   one: 2,
-  fizz: 'buzz',
-  buzz: true
+  buzz: true,
+  arr: [1, 2, 3]
 };
 
 const example2 = {
-  fizz: 'buzz',
   one: 2,
-  buzz: true,
-  //talk: 'mentor'
+  fizz: 'buzz',
+  arr: [1, 2, 3],
+  buzz: true
 };
 
 const example3 = {
