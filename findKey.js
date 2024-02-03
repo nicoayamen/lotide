56db1afd2d94e27d653ff0d6eecc639523c8f757
nicoayamen <nicolas.ayala4@gmail.com>
2024-02-02 21:15:25 -0700

    created func to loop an obj, with callback, and return key

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const findKey = function(obj, call) {
+  let result = undefined;
+  for (key in obj) {
+    if (call(obj[key])) {
+      result = [key];
+      break;
+    } else {
+        undefined;
+    }
+  }
+  return result.toString();
+}
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 5 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "Ora") // => "noma"
