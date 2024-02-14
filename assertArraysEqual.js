dcecbbb7f6ac2063ee010433393d17f00f7106d2
nicoayamen <nicolas.ayala4@gmail.com>
2024-02-13 18:24:15 -0700

    refactored assertArraysEquals and made test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-// func to show if two arrs have equal elements/length
-const eqArrays = function(arr1, arr2) {
-  // checks to see arrs do not have the same length
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; arr1.length > i; i++) {
-    // checks to see if, as the arrs are looping, are not equal
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-
-  }
-  // to break the func if all upper if dont trigger
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(a, b) {
   if (!eqArrays(a, b)) {
@@ -22,4 +7,5 @@ const assertArraysEqual = function(a, b) {
   console.log(`Assertion Passed: ${a} === ${b}`);
   }
 }
-assertArraysEqual([1, 2, "3"], [1, 2, 3]);
+
+module.exports = assertArraysEqual;
