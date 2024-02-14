51a608463ea00c6f1856b4ad64287bca3d107f8d
nicoayamen <nicolas.ayala4@gmail.com>
2024-02-13 18:15:49 -0700

    refactored eqArrays and added test

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// assertion function to check if param actual and expected are strictly =
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // func to show if two arrs have equal elements/length
 const eqArrays = function(arr1, arr2) {
   // checks to see arrs do not have the same length
@@ -24,5 +15,5 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-assertEqual(eqArrays([1, 2, 3], [1, "2", 3]), true);
+module.exports = eqArrays;
+
