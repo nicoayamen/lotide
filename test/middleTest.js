b72ce33d75ab71c0359d5372d384a9cd36e4f698
nicoayamen <nicolas.ayala4@gmail.com>
2024-02-13 19:20:46 -0700

    used chai for test for tail and middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
+describe('#middle', () => {
+  it("returns [2, 3] from [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  })
+  it("retuns [2] from [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  })
+})
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
+
+
+
+//assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+//assertArraysEqual(middle([1, 2, 3]), [2]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,24 +1,12 @@
 const tail = require('../tail')
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert
 
+describe('#tail', () => {
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]', () => {
+  assert.deepEqual(tail(["Yo Yo" ,"Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  })
+  it('returns [] for ["lighthouse"]', () => {
+    assert.deepEqual(tail(["lighthouse"]), []);
+  })
 
-
-
-
-
-
-//test case 1
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labss"); // ensure second element is "Labs"
-
-//test case 2
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(words); //checking to see original array
-console.log(tail(words)); // checking return in new array
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// test case 3
-assertEqual(tail(["test"]).length, 0); // tail of array with one element only should be empty
-assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
+})
