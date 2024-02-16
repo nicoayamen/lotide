2d13fd51aca8292ecbf32679a1ac812a3b41aa23
nicoayamen <nicolas.ayala4@gmail.com>
2024-02-15 17:40:41 -0700

    added comments in takeUntil and map.

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 const map = function(arr, call) {
 const results = [];
+// loops thru the key of input arr and pushes it to results using callback
   for (item of arr) {
     results.push(call(item));
   }
--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,7 @@
 const takeUntil = function(arr, call) {
   const results = [];
   for (item of arr) {
+    // uses callback conitional here to push the item in func call to results arr
     if (call(item) === false) {
       results.push(item);
     } else {
